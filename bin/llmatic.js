ae2e0a7f12a47b5ea3e8774eace59e2af2c486b0
Fardjad Davari <public@fardjad.com>
2023-05-27 00:16:29 +0200

    Fix cli scripts

---
+++ bin/llmatic.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import childProcess from "node:child_process";
+
+childProcess.fork(
+  new URL("../src/cli/llmatic.ts", import.meta.url),
+  process.argv.slice(2),
+  {
+    execArgv: ["--no-warnings", "--loader", "tsx"],
+  }
+);
