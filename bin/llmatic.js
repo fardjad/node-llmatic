ede3447537991ccf2511f03b17f718ce45336a8b
Fardjad Davari <public@fardjad.com>
2023-10-19 01:19:07 +0200

    Fix compatibility issues with Node 20+

--- bin/llmatic.js
+++ bin/llmatic.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import childProcess from "node:child_process";
+import { createRequire } from "node:module";
+import { fileURLToPath } from "node:url";
 
-childProcess.fork(
-  new URL("../src/cli/llmatic.ts", import.meta.url),
-  process.argv.slice(2),
-  {
-    execArgv: ["--no-warnings", "--loader", "tsx"],
-  },
-);
+const tsx =
+  import.meta.resolve &&
+  // This can be removed once node 18 is EOL
+  import.meta.resolve.constructor.name !== "AsyncFunction"
+    ? fileURLToPath(import.meta.resolve("tsx/cli"))
+    : createRequire(import.meta.url).resolve("tsx/cli");
+
+childProcess.fork(tsx, [
+  "--no-warnings",
+  fileURLToPath(new URL("../src/cli/llmatic.ts", import.meta.url)),
+  ...process.argv.slice(2),
+]);
